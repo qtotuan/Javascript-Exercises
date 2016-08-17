612f59667907c25eb8f99f6e085301773d87e1a5
qtotuan <quynh.totuan@gmail.com>
2016-08-17 16:41:04 -0400

    The 2nd set of homework answers are complete.

--- solutions.js
+++ solutions.js
@@ -1,5 +1,7 @@
-var nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+/* 1st set of homework */
 
+// ARRAYS
+var nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
 nums.sort();
 var halfArray = Math.floor(nums.length/2);
 //console.log(nums.length); => seems like there are 25 array items
@@ -7,6 +9,7 @@ var halfArray = Math.floor(nums.length/2);
 console.log(nums[halfArray+1]);
 
 
+// OBJECTS
 var obj = {
    name: "Slimer",
    color: "greenish",
@@ -18,3 +21,41 @@ obj.type = "creature";
 console.log(obj.type);
 obj.age = 6;
 console.log(obj.age);
+
+
+
+/* 2nd set of homework */
+
+// LOOP 1
+var eulerArray = [];
+for (i = 0; i < 1000; i++) {
+  if (i % 3 == 0 || i % 5 == 0) {
+    eulerArray.push(i);
+  }
+}
+//console.log(eulerArray);
+var total = 0;
+for (var i = 0; i < eulerArray.length; i++) {
+  total = total + eulerArray[i];
+}
+console.log(total);
+
+
+// LOOP 2
+var Wilkersons = ["Lois", "Dewie", "Francis", "Malcolm", "Reese", "Hal"];
+var halfArray = Math.floor(Wilkersons.length/2);
+console.log("There are " + Wilkersons.length + " Wilkersons.")
+console.log(halfArray);
+for (var i = halfArray; i < Wilkersons.length; i++) {
+  console.log(Wilkersons[i]);
+}
+
+
+// LOOP 3
+var plusJamie = ["Lois", "Dewie", "Francis", "Malcolm", "Reese", "Hal", "Jamie"];
+var halfArray = Math.floor(plusJamie.length/2);
+console.log("There are " + plusJamie.length + " Wilkersons plus Jamie.")
+console.log(halfArray);
+for (var i = halfArray; i < plusJamie.length; i++) {
+  console.log(plusJamie[i]);
+}
